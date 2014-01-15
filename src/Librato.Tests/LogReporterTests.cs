5975e12ba6e488b8b69d6b093894865974fb93e5
Rune <rs@appharbor.com>
2014-01-14 23:01:56 -0800

    Verify that LogReporter#Increment writes a CountMetric

--- src/Librato.Tests/LogReporterTests.cs
+++ src/Librato.Tests/LogReporterTests.cs
@@ -1,6 +1,19 @@
-﻿namespace LogReporter.Tests
+﻿using Moq;
+using Xunit;
+
+namespace Librato.Tests
 {
 	public class LogReporterTests
 	{
+		[Fact]
+		public void ShouldWriteCountMetricWhenIncrementing()
+		{
+			var metricWriterMock = new Mock<IMetricWriter>(MockBehavior.Loose);
+			var logReporter = new LogReporter(metricWriterMock.Object);
+
+			logReporter.Increment("foo");
+
+			metricWriterMock.Verify(x => x.Write(It.IsAny<CountMetric>()));
+		}
 	}
 }
