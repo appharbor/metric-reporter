dc16a314723ebcd6b80e942e54ac01dd8d4c6ba7
Rune <rs@appharbor.com>
2014-01-15 04:22:09 -0800

    Add sample showing MetricReporter usage

--- src/Librato.Sample/Program.cs
+++ src/Librato.Sample/Program.cs
@@ -1,9 +1,36 @@
-﻿namespace Librato.Sample
+﻿using System;
+using System.Threading;
+
+namespace Librato.Sample
 {
 	public class Program
 	{
 		public static void Main()
 		{
+			var writer = new L2MetWriter(Console.Out);
+			var reporter = new MetricReporter(writer);
+
+			// Increment count by one
+			reporter.Increment("users");
+
+			// Increment by 8 and set source
+			reporter.Increment("products", incrementBy: 8, source: "web.1");
+
+			// Measure time to complete a task
+			reporter.Measure("search.querytime", () =>
+			{
+				// Do work
+				Thread.Sleep(1000);
+			});
+
+			// Add a prefix to a group of metrics
+			reporter.Group("eu-region", x =>
+			{
+				x.Increment("sessions", 45, "web.2");
+				x.Measure("db.execution_time", 344, "web.2");
+			});
+
+			Console.ReadLine();
 		}
 	}
 }
