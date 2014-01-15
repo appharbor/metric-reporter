7026ed131d47515a0add70a063e89d6a63dccc2f
Rune <rs@appharbor.com>
2014-01-15 00:10:11 -0800

    Verify that MeasureMetric sets MetricType to Measure

--- src/Librato.Tests/MeasureMetricTests.cs
+++ src/Librato.Tests/MeasureMetricTests.cs
@@ -1,6 +1,15 @@
-﻿namespace Librato.Tests
+﻿using Xunit;
+
+namespace Librato.Tests
 {
 	public class MeasureMetricTests
 	{
+		[Fact]
+		public void ShouldSetMetricTypeToMeasure()
+		{
+			var measureMetric = new MeasureMetric("foo", 1);
+
+			Assert.Equal(MetricType.Measure, measureMetric.MetricType);
+		}
 	}
 }
