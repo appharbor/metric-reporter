9741763e3e5d19652a962ade6c24c63f4c0ed3fa
Rune <rs@appharbor.com>
2014-01-14 23:48:31 -0800

    Implement L2MetWriter#Write

--- src/Librato.Tests/L2MetWriterTests.cs
+++ src/Librato.Tests/L2MetWriterTests.cs
@@ -1,6 +1,30 @@
-﻿namespace Librato.Tests
+﻿using System.IO;
+using Moq;
+using Xunit;
+
+namespace Librato.Tests
 {
 	public class L2MetWriterTests
 	{
+		private readonly Mock<TextWriter> _textWriterMock;
+		private readonly L2MetWriter _l2MetWriter;
+
+		public L2MetWriterTests()
+		{
+			_textWriterMock = new Mock<TextWriter>(MockBehavior.Loose);
+			_l2MetWriter = new L2MetWriter(_textWriterMock.Object);
+		}
+
+		[Fact]
+		public void ShouldWriteL2MetMetrics()
+		{
+			var metricName = "foo";
+			var metricValue = 123;
+
+			var metric = new CountMetric(metricName, metricValue);
+			_l2MetWriter.Write(metric);
+
+			_textWriterMock.Verify(x => x.WriteLine(string.Format("{0}#{1}={2}", "count", metricName, metricValue)));
+		}
 	}
 }
--- src/Librato/L2MetWriter.cs
+++ src/Librato/L2MetWriter.cs
@@ -1,12 +1,30 @@
 ﻿using System;
+using System.IO;
 
 namespace Librato
 {
 	public class L2MetWriter : IMetricWriter
 	{
+		private readonly TextWriter _textWriter;
+
+		public L2MetWriter(TextWriter textWriter)
+		{
+			_textWriter = textWriter;
+		}
+
 		public void Write(Metric metric)
 		{
-			throw new NotImplementedException();
+			var l2MetType = GetL2MetType(metric);
+			_textWriter.WriteLine(string.Format("{0}#{1}={2}", l2MetType, metric.Name, metric.Value));
+		}
+
+		private static string GetL2MetType(Metric metric)
+		{
+			if (metric is CountMetric)
+			{
+				return "count";
+			}
+			throw new NotSupportedException();
 		}
 	}
 }
