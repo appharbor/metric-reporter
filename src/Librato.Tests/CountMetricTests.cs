b72b8b05f3f888c93ae1a6f4bedc359222d64990
Rune <rs@appharbor.com>
2014-01-15 00:04:42 -0800

    Verify that CountMetric set metric type to Count

--- src/Librato.Tests/CountMetricTests.cs
+++ src/Librato.Tests/CountMetricTests.cs
@@ -1,6 +1,14 @@
-﻿namespace Librato.Tests
+﻿using Xunit;
+
+namespace Librato.Tests
 {
 	public class CountMetricTests
 	{
+		[Fact]
+		public void ShouldSetMetricTypeToCount()
+		{
+			var countMetric = new CountMetric("foo", 1);
+			Assert.Equal(MetricType.Count, countMetric.MetricType);
+		}
 	}
 }
