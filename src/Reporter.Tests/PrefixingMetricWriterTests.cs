0484c5e426726271ff4fd9c2cf52b8cc1acf4095
Rune <rs@appharbor.com>
2014-01-18 16:38:37 -0800

    Verify that a metric is added when writing

--- src/Reporter.Tests/PrefixingMetricWriterTests.cs
+++ src/Reporter.Tests/PrefixingMetricWriterTests.cs
@@ -1,6 +1,22 @@
-﻿namespace AppHarbor.Metrics.Reporter.Tests
+﻿using System.Linq;
+using Moq;
+using Xunit;
+
+namespace AppHarbor.Metrics.Reporter.Tests
 {
 	public class PrefixingMetricWriterTests
 	{
+		[Fact]
+		public void ShouldAddPrefixToMetric()
+		{
+			var prefix = "foo";
+			var metric = new CounterMetric("bar", 1);
+			var writer = new PrefixingMetricWriter(prefix, new Mock<IMetricWriter>().Object);
+
+			writer.Write(metric, null);
+
+			Assert.Single(metric.Prefixes);
+			Assert.Equal(metric.Prefixes.Single(), prefix);
+		}
 	}
 }
